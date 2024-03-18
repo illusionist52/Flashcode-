0268bb383d77f6b37afb55f63bc8eb61b1dd9e76
usmaan <2021.mohdusmaan.mogal@ves.ac.in>
2024-03-18 11:37:02 +0530

    login demo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Homepage from "./pages/Homepage"
+import Login from "./pages/Login"
 
 export default function App() {
   return (
    <BrowserRouter>
    <Routes>
     <Route path="/" element={<Homepage/>} />
+    <Route path="/login" element={<Login/>} />
    </Routes>
    </BrowserRouter>
   )
